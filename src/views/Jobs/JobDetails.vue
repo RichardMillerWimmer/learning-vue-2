47f26c7dd4157a901aeb0848a0951be1296b23ff
Richard Miller <richardmmill@gmail.com>
2021-06-09 18:51:52 -0400

    json-server, fetch in vue, conditional render with v-if/v-else

--- src/views/Jobs/JobDetails.vue
+++ src/views/Jobs/JobDetails.vue
@@ -1,7 +1,13 @@
 <template>
-  <h1>Job Details Page</h1>
-  <!-- <p>The job ID is {{ $route.params.id}}</p>     syntax for use with route object -->
-  <p>The job ID is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <!-- <p>The job ID is {{ $route.params.id}}</p>     syntax for use with route object -->
+    <p>The job ID is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job...</p>
+  </div>
 </template>
 
 <script>
@@ -10,7 +16,24 @@ export default {
   data() {
     return {
       // id: this.$route.params.id ---> this is now set up as prop
+      job: null,
+      // THIS WORKS for null render error on mount without using v-if
+      // job: {
+      //   title: '',
+      //   id: this.id,
+      //   details: ''
+      // }
     };
+  },
+  mounted() {
+    // console.log("job mount");
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => {
+        (this.job = data)
+        // console.log(this.job)
+        })
+      .catch((error) => console.log(error.message));
   },
 };
 </script>
--- src/views/Jobs/Jobs.vue
+++ src/views/Jobs/Jobs.vue
@@ -1,7 +1,14 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
-      <router-link :to="{ name: 'JobDetails', params: {id: job.id}}"><h2>{{job.title}}</h2></router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }"
+        ><h2>{{ job.title }}</h2></router-link
+      >
+    </div>
+  </div>
+  <div v-else>
+    <p>Jobs loading...</p>
   </div>
 </template>
 
@@ -9,13 +16,15 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: "UX Designer", id: 0, details: "designs UX" },
-        { title: "Web Developer", id: 1, details: "developes web apps" },
-        { title: "Vue Developer", id: 2, details: "developes Vue apps" },
-        { title: "React Developer", id: 3, details: "developes React apps" },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    // console.log('jobs mount')
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((error) => console.log(error.message));
   },
 };
 </script>
